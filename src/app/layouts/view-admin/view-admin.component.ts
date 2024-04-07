c448a3b5b7a1ddaa74347f87c585276c2e7e4d26
mungnxv7 <mungnxhnv7@gmail.com>
2024-04-07 19:08:16 +0700

    update

--- src/app/layouts/view-admin/view-admin.component.ts
+++ src/app/layouts/view-admin/view-admin.component.ts
@@ -3,27 +3,33 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
 import { userLocal } from '../../../config/viewLocal';
 import { AdminNavbarComponent } from '../../components/admin-navbar/admin-navbar.component';
-
+import { ActivatedRoute } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 
 @Component({
   selector: 'app-view-admin',
   standalone: true,
-  imports: [RouterOutlet,AdminNavbarComponent,RouterLink],
+  imports: [RouterOutlet,AdminNavbarComponent,RouterLink,NgClass],
   templateUrl: './view-admin.component.html',
   styleUrl: './view-admin.component.css'
 })
 export class ViewAdminComponent {
   router = inject(Router)
-
+  activateRoute = inject(ActivatedRoute)
+  active=""
+  isActive(url: string): boolean {
+    return this.router.url.startsWith(url);
+  }
   ngOnInit(){
     const data = userLocal.getUserLoacal()
     if(data){
       if(data.user.role !== 'admin'){
         this.router.navigateByUrl('')
       }
-     }else{
-      this.router.navigateByUrl('')
      }
+
+     
+    
   }
 }
